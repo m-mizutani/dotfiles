ae73df91b895ce3886f599dc7f58bd820e2e0e44
Masayoshi Mizutani <mizutani@cookpad.com>
2018-01-16 13:28:22 +0900

    Add if-then for pyenv + rbenv

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,8 +1,15 @@
 alias em="emacs -nw"
 alias make="make -j8"
-rbenv init - | source
-set -g -x GOPATH $HOME/godev
-set -x PATH $HOME/.pyenv/shims $PATH
-. (pyenv init - | psub)
+
+if test -e $HOME/.rbenv
+  # rbenv init - | source
+end
+
+if test -e $HOME/.pyenv
+  set -x PATH $HOME/.pyenv/shims $PATH
+  . (pyenv init - | psub)
+end
+
 set -x GOPATH $HOME/dev/go
+set -x PATH /opt/brew/bin $PATH
 
