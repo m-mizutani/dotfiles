43db77e1cd7c434b56ba6f3c9c945a92f7e70467
Masayoshi Mizutani <mizutani@cookpad.com>
2021-07-03 10:50:46 +0900

    updated

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,15 +1,13 @@
 alias em="emacs -nw"
 alias make="make -j8"
-alias gcd="cd (ghq root)/(ghq list | peco)"
+alias gcd="cd (ghq root)/(ghq list | sort | peco)"
 alias gh="hub browse (ghq list | peco | cut -d '/' -f 2,3)"
 alias jqc="jq --color-output"
 alias less="less -r"
 alias l="less -r"
+alias pb="pbpaste | pbcopy"
 
 
-set -x GOPATH $HOME/dev/go
-mkdir -p $GOPATH/bin
-set -x PATH $GOPATH/bin $PATH
 set -x GO111MODULE auto
 
 if test -e $HOME/local/bin
@@ -40,6 +38,14 @@ if test -e $HOME/.rbenv
   # $HOME/.rbenv/bin/rbenv init - | source
 end
 
+if test -e $HOME/.goenv
+  set -x GOENV_ROOT $HOME/.goenv
+  set -x PATH $GOENV_ROOT/bin $PATH
+  status --is-interactive; and source (goenv init -|psub)
+  set -x PATH $GOROOT/bin $PATH
+  set -x PATH $GOPATH/bin $PATH
+end
+
 if test -e $HOME/.cargo/env
   . $HOME/.cargo/env
 end
@@ -49,7 +55,7 @@ if test -e /usr/local/texlive/2018/bin/x86_64-darwin
 end
 
 
-if test -e $HOME/.cpad2/profile.fish 
+if test -e $HOME/.cpad2/profile.fish
   . ~/.cpad2/profile.fish
 end
 
